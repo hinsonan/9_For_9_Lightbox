1643392cbfc81c94045b97af9385bb8c3315f39c
Andrew Hinson <hinsonan@gmail.com>
2017-11-04 15:14:47 -0500

    Adds basic layout and classes

---
+++ 9_For_9/9_For_9/MeetTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MeetTableViewCell.swift
+//  9_For_9
+//
+//  Created by Andrew Hinson on 11/4/17.
+//  Copyright © 2017 Andrew Hinson. All rights reserved.
+//
+
+import UIKit
+
+class MeetTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var meetCellImage: UIImageView!
+    @IBOutlet weak var meetCellName: UILabel!
+    @IBOutlet weak var meetCellLocation: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
