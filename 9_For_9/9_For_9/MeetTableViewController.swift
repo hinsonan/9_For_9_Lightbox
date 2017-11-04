763f60a155132ba92882e3ca5054a380e6f7be11
Andrew Hinson <hinsonan@gmail.com>
2017-11-04 16:56:14 -0500

    Fixes stupid constraint issue with cells

--- 9_For_9/9_For_9/MeetTableViewController.swift
+++ 9_For_9/9_For_9/MeetTableViewController.swift
@@ -50,18 +50,18 @@ class MeetTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return 3
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "meetCell", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
