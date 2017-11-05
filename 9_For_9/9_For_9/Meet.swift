968941dda1cd79cb776a56f25c58c39f77e805e4
Andrew Hinson <hinsonan@gmail.com>
2017-11-04 20:27:26 -0500

    Add model

---
+++ 9_For_9/9_For_9/Meet.swift
@@ -0,0 +1,18 @@
+//
+//  Meet.swift
+//  9_For_9
+//
+//  Created by Andrew Hinson on 11/4/17.
+//  Copyright © 2017 Andrew Hinson. All rights reserved.
+//
+
+import Foundation
+
+class Meet{
+    var Name: String?
+    var Location: String?
+    var ImgName: String?
+    var ImgURL: String?
+    var Password: String?
+    var Creater: String?
+}
