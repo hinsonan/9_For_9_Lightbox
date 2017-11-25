14630d4b99f1dcce5149efd3a99c495cc3945526
Andrew Hinson <hinsonan@gmail.com>
2017-11-24 20:50:05 -0500

    Adds the start of create view

---
+++ 9_For_9/9_For_9/CreateMeetViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateMeetViewController.swift
+//  9_For_9
+//
+//  Created by Andrew Hinson on 11/24/17.
+//  Copyright © 2017 Andrew Hinson. All rights reserved.
+//
+
+import UIKit
+
+class CreateMeetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
